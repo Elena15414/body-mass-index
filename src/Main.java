b564a8eae3ad645ba5ff7fda2f4ccfe11afecee2
Elena Datsenko <Elenasheglova15414@Gmail.com>
2023-02-17 01:45:23 +0300

    readme

--- src/Main.java
+++ src/Main.java
@@ -7,3 +7,8 @@ public class Main {
         System.out.println(index);
     }
 }
+ // Консоль :
+// "C:\Program Files\Eclipse Adoptium\jdk-11.0.17.8-hotspot\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.2\lib\idea_rt.jar=64946:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.2\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\Татьяна\body mass index\out\production\body mass index" Main
+//19
+//
+//Process finished with exit code 0
